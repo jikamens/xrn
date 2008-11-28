3f4f17ab5ab0c504476d993b374d74df13e7e943
Jonathan Kamens <jik@quantopian.com>
2008-11-28 18:31:04 +0000

    Move XRN stuff into its own directory

--- activecache.h
+++ activecache.h
@@ -6,6 +6,6 @@
 
 extern void active_cache_read _ARGUMENTS((char *));
 extern int active_cache_write _ARGUMENTS((char *, struct newsgroup **,
-					  ng_num, Boolean));
+					  /* ng_num */ int, /* Boolean */ int));
 
 #endif /* _ACTIVECACHE_H_ */
