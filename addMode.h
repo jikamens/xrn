6dbd9e8d2a279342576c7a7423942460f982ebd9
Jonathan Kamens <jik@quantopian.com>
2008-11-28 18:31:04 +0000

    Move XRN stuff into its own directory

--- addMode.h
+++ addMode.h
@@ -6,20 +6,18 @@
 #include "butdefs.h"
 
 BUTDECL(addQuit);
-BUTDECL(addIgnoreRest);
 BUTDECL(addFirst);
 BUTDECL(addLast);
 BUTDECL(addAfter);
 BUTDECL(addUnsub);
-BUTDECL(addIgnore);
 
 extern XtActionsRec AddActions[];
 extern int AddActionsCount;
 
+extern ButtonList AddButtonList[];
+extern int AddButtonListCount;
+
 extern void redrawAddTextWidget _ARGUMENTS((String, long));
 extern void switchToAddMode _ARGUMENTS((String));
-
-extern void displayAddWidgets _ARGUMENTS((void));
-extern void hideAddWidgets _ARGUMENTS(());
 
 #endif /* ADDMODE_H */
